2b6f4d7a659d488e1864232258e8441af55160cb
Per H <per_hakansson@live.se>
2021-06-21 10:03:40 +0200

    new method

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
 
+        int[] array = {3, 5, 2, 1, 7};
+        System.out.println(Arrays.toString(sortList(array)));
+    }
+
+    public static int[] sortList (int[] arr){
+
+        Arrays.sort(arr);
+        return arr;
     }
 }
