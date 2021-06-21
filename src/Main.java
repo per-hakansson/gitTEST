c7aeea51bddf5758dc635f30cf1b3abece705323
Per H <per_hakansson@live.se>
2021-06-21 10:36:20 +0200

    New method

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
 
+    }
+
+    public static int[] sortDecended(int[] arr) {
+
+        Arrays.sort(arr);
+
+        return arr;
     }
 }
